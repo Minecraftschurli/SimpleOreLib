ed9f37ae08ce0d9eb5691a12aa7364c655274dfe
Minecraftschurli <minecraftschurli@gmail.com>
2020-04-24 20:56:10 +0200

    make thread-safe
    Signed-off-by: Minecraftschurli <minecraftschurli@gmail.com>

--- src/main/java/minecraftschurli/simpleorelib/SimpleOreLib.java
+++ src/main/java/minecraftschurli/simpleorelib/SimpleOreLib.java
@@ -7,16 +7,16 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
 import net.minecraft.world.gen.placement.CountRangeConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.apache.commons.compress.archivers.zip.UnsupportedZipFeatureException;
 
 import java.util.*;
 
@@ -35,47 +35,53 @@ public final class SimpleOreLib {
     private static final Map<BiomeFilter, OreFeatureConfig.FillerBlockType> fillers = new HashMap<>(new ImmutableMap.Builder<BiomeFilter, OreFeatureConfig.FillerBlockType>().put(BiomeFilter.NETHER, OreFeatureConfig.FillerBlockType.NETHERRACK).put(BiomeFilter.THE_END, END_STONE).build());
 
 
-    public SimpleOreLib (){
+    public SimpleOreLib() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        ForgeRegistries.BIOMES.forEach(biome -> {
+        DeferredWorkQueue.runLater(this::registerOres);
+    }
+
+    private void registerOres() {
+        for (Biome biome : ForgeRegistries.BIOMES) {
             for (Ore ore : ORES) {
-                if (ore.hasBiomeFilter() && !ore.getBiomeFilter().apply(biome))
-                    continue;
-                biome.addFeature(
-                        GenerationStage.Decoration.UNDERGROUND_ORES,
-                        Feature.ORE
-                                .withConfiguration(
-                                        new OreFeatureConfig(
-                                                fillerForBiome(biome),
-                                                ore.getGenerationState(biome),
-                                                ore.getVeinSize()))
-                                .func_227228_a_(
-                                        Placement.COUNT_RANGE
-                                                .func_227446_a_(
-                                                        new CountRangeConfig(
-                                                                ore.getChance(),
-                                                                ore.getMinHeight(),
-                                                                ore.getMinHeight(),
-                                                                ore.getMaxHeight()))));
+                if (ore.hasBiomeFilter() && !ore.getBiomeFilter().apply(biome)) continue;
+
+                CountRangeConfig placementConfig = new CountRangeConfig(
+                        ore.getChance(),
+                        ore.getMinHeight(),
+                        ore.getMinHeight(),
+                        ore.getMaxHeight());
+
+                ConfiguredPlacement<CountRangeConfig> placement = Placement.COUNT_RANGE.func_227446_a_(placementConfig);
+
+                OreFeatureConfig featureConfig = new OreFeatureConfig(
+                        fillerForBiome(biome),
+                        ore.getGenerationState(biome),
+                        ore.getVeinSize());
+
+                ConfiguredFeature<?, ?> feature = Feature.ORE.withConfiguration(featureConfig).func_227228_a_(placement);
+
+                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, feature);
             }
-        });
+        }
     }
 
     /**
      * Registeres a {@link net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType} for the given {@link BiomeFilter}
+     *
      * @param filter the {@link BiomeFilter} to register the {@link net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType filler} for
      * @param filler the {@link net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType filler} to register
      */
-    public static void addFiller(BiomeFilter filter, OreFeatureConfig.FillerBlockType filler){
+    public static void addFiller(BiomeFilter filter, OreFeatureConfig.FillerBlockType filler) {
         fillers.putIfAbsent(filter, filler);
     }
 
     /**
      * Gets the {@link net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType filler} for the given {@link Biome}
+     *
      * @param biome the {@link Biome} to get the {@link net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType filler} for
      * @return the {@link net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType filler} for the given {@link Biome}
      */
